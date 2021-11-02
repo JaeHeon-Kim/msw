30bb54d08c3e9fb918a801eacb09b6c9ef79322f
Jay <user01@Jays-MacBookPro.local>
2021-11-02 18:43:54 +0900

    /

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
 function App() {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts/101')
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setUser(data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <code>{JSON.stringify(user, undefined, 2)}</code>{' '}
       </header>
     </div>
   );
